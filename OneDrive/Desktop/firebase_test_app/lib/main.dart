78e416ccc43ff6ce2f90d2706d1e18deaf175541
Dikshant <dikshantahalawat2005@gmail.com>
2024-10-09 11:06:09 +0530

    Home screen after opening the app when logged in fixed

--- OneDrive/Desktop/firebase_test_app/lib/main.dart
+++ OneDrive/Desktop/firebase_test_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_test_app/screens/home_screen.dart';
 import 'package:firebase_test_app/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +19,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Auth_App',
-      home: const SigninScreen(),//after opening app sign in screen appear
-    );
+      home: FutureBuilder<User?>(future: FirebaseAuth.instance.authStateChanges().first,
+      builder: (context , snapshot){
+
+      if(snapshot.hasData){
+        return const HomeScreen();
+      }
+      else{
+        return const SigninScreen();
+      }
+      }
+      
+    ));
   }
 }
